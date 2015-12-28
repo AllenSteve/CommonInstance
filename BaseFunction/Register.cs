8a271135c18e9a4dc546db841b41d9d6c83410d1
AllenSteve <lacrima2012@qq.com>
2015-12-28 14:50:49 +0800

    01.更新Ninject注册类；

--- BaseFunction/Register.cs
+++ BaseFunction/Register.cs
@@ -10,26 +10,26 @@ namespace BaseFunction
 {
     public class Register
     {
-        private StandardKernel _kernel = new StandardKernel();
+        private IKernel kernel { get; set; }
 
         // 以后这里就不用更改这里了，只需要该xml文件就可以了
         public Register()
         {
             var settings = new NinjectSettings() { LoadExtensions = false };
-            _kernel = new StandardKernel(settings, new XmlExtensionModule());
+            this.kernel = new StandardKernel(settings, new XmlExtensionModule());
             //_kernel.Load("Xml/Register.xml");
-            _kernel.Load("../../NinjectServiceModule.xml");
+            this.kernel.Load("../../NinjectServiceModule.xml");
         }
 
         //获取
         public TInterface Get<TInterface>()
         {
-            return _kernel.Get<TInterface>();
+            return kernel.Get<TInterface>();
         }
 
         public void Dispose()
         {
-            _kernel.Dispose();
+            kernel.Dispose();
         }
     }
 }
