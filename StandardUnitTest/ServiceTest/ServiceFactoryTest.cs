2fbbe6d2393dda5c786986f10c529b544e56b08c
AllenSteve <lacrima2012@qq.com>
2015-12-28 14:44:26 +0800

    01.更改配置文件； 02.更新工厂类单元测试；

--- StandardUnitTest/ServiceTest/ServiceFactoryTest.cs
+++ StandardUnitTest/ServiceTest/ServiceFactoryTest.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BaseFunction.ServiceInterface;
 using BaseFunction.Service;
+using BaseFunction.Service.BaseService;
+using BaseFunction;
 
 namespace StandardUnitTest.ServiceTest
 {
@@ -10,20 +12,31 @@ namespace StandardUnitTest.ServiceTest
     {
         private IServiceFactory factory { get; set; }
 
+        private NinjectModuleFactory ninjectFactory { get; set; }
+
         private IBaseService service { get; set; }
 
         public ServiceFactoryTest()
         {
             this.factory = new ServiceFactory();
+            this.ninjectFactory = new NinjectModuleFactory();
         }
 
 
         [TestMethod]
         public void CreateTest()
         {
-            service = factory.CreateInstance<DateService>();
+            service = factory.CreateInstance<IBaseService>();
             var dateService = factory.CreateInstance<DateService>();
             var date = dateService.GetMonthFifthWorkingDay("201512");
+        }
+
+        [TestMethod]
+        public void CreateNinjectModuleFactoryTest()
+        {
+            IDateService ds = factory.CreateInterface<IDateService>();
+
+            ds = ninjectFactory.CreateInterface<IDateService>();
         }
     }
 }
