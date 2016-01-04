410429fac3bd16be1bd59bad5535a5ca43117a9b
AllenSteve <lacrima2012@qq.com>
2016-01-04 22:01:55 +0800

    提交单元测试；

--- StandardUnitTest/ServiceTest/EbsServiceTest/TokenMethodTest.cs
+++ StandardUnitTest/ServiceTest/EbsServiceTest/TokenMethodTest.cs
@@ -18,6 +18,8 @@ namespace StandardUnitTest.ServiceTest.EbsServiceTest
         public void CreateCryptographTest()
         {
             string timestamp = "1451875269";
+            timestamp = TokenMethod.ConvertDateTime2Timestamp(DateTime.Now).ToString();
+            timestamp = "1451902055";
             string soufunId = "54090696";
             string cryptograph = TokenMethod.CreateCryptograph(timestamp, soufunId);
             bool result = method.VerifyParam(soufunId, cryptograph.Replace("%2B", "+"), new TimeSpan(TimeSpan.TicksPerDay * 5));
@@ -28,6 +30,7 @@ namespace StandardUnitTest.ServiceTest.EbsServiceTest
         {
             string cryptograph = "rzcssbtvCfz6kCM5B7XQeS9jWISTaLXe/BqGLiGeVICOkJsDEq7Fwe3oFt/S9Pff";
             cryptograph = "xG/pk85t9Lfw2HLsGF8W8HJBGcje55WeQDXzkB1CIvZ9MMFNLh2xJbY9cP2AV5i0";
+            cryptograph = "EIkfoBi7ws7u2kS0adOFemx5J8vMX7gUKjeVvB9VX/lq1/9LafIpoxVL8NXuko41";
             string plaintext = AdvancedEncryption.AESDecrypt(cryptograph);
         }
     }
