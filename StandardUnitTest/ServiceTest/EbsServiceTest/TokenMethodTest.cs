a1024ca0ac2fb3f717bbace52176dfca5cffe45b
AllenSteve <lacrima2012@qq.com>
2016-01-04 11:29:02 +0800

    01.新增配置文件； 02.新增单元测试方法；

---
+++ StandardUnitTest/ServiceTest/EbsServiceTest/TokenMethodTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BaseFunction.Service.EbsService;
+
+namespace StandardUnitTest.ServiceTest.EbsServiceTest
+{
+    [TestClass]
+    public class TokenMethodTest
+    {
+        private TokenMethod method { get; set; }
+
+        public TokenMethodTest()
+        {
+            this.method = new TokenMethod();
+        }
+
+        [TestMethod]
+        public void CreateCryptographTest()
+        {
+            string timestamp = "1451875269";
+            string soufunId = "54090696";
+            string cryptograph = TokenMethod.CreateCryptograph(timestamp, soufunId);
+            bool result = method.VerifyParam(soufunId, cryptograph.Replace("%2B", "+"), new TimeSpan(TimeSpan.TicksPerDay * 5));
+        }
+
+        [TestMethod]
+        public void CryptographTest()
+        {
+            string cryptograph = "rzcssbtvCfz6kCM5B7XQeS9jWISTaLXe/BqGLiGeVICOkJsDEq7Fwe3oFt/S9Pff";
+            cryptograph = "xG/pk85t9Lfw2HLsGF8W8HJBGcje55WeQDXzkB1CIvZ9MMFNLh2xJbY9cP2AV5i0";
+            string plaintext = AdvancedEncryption.AESDecrypt(cryptograph);
+        }
+    }
+}
