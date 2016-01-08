053a2458b5b6dadc87db9ec00121a912674befa4
AllenSteve <lacrima2012@qq.com>
2016-01-08 16:35:57 +0800

    01.新增测试方法；

--- WebTools/Index.aspx.cs
+++ WebTools/Index.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EBS.Interface.EContract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,10 @@ namespace EOP.Web.Ajax
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //HttpContext context = new HttpContext(Request,new HttpResponse(new System.IO.StringWriter()));
+            var contract = new GetUnsignedConstructionContract();
+            contract.ProcessRequest(base.Context);
+            base.Context.Response.End();
         }
     }
 }
