ac7ac819ea82fe5e7bdac04cd702c331a1f2d825
AllenSteve <lacrima2012@qq.com>
2016-01-18 10:57:09 +0800

    新增distinct扩展方法；

--- ExtensionComponent/DistinctExtensions.cs
+++ ExtensionComponent/DistinctExtensions.cs
@@ -12,5 +12,24 @@ namespace ExtensionComponent
         {
             return source.Distinct(new CommonEqualityComparer<T, V>(keySelector));
         }
+        /// <summary>
+        /// http://www.cnblogs.com/A_ming/archive/2013/05/24/3097062.html
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
     }
 }
