944ae3aa7b35f0d4659f98e42341a7752abb6944
AllenSteve <lacrima2012@qq.com>
2016-01-18 11:21:58 +0800

    01.新增类文件；

---
+++ EBSComponent/Model/Entity/C_SMSInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentModels.EbsDBModel
+{
+    public partial class C_SMSInfo
+    {
+        public int Id { get; set; }
+        public string OrderId { get; set; }
+        public long SoufunId { get; set; }
+        public string Phone { get; set; }
+        public string SMSChannel { get; set; }
+        public string SMSContent { get; set; }
+        public System.DateTime SendTime { get; set; }
+        public System.DateTime CreateTime { get; set; }
+        public int SendState { get; set; }
+        public int IsDel { get; set; }
+    }
+}
---
+++ EBSComponent/Model/Entity/OrderFollowUpEvaluateLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentModels.EbsDBModel
+{
+    public partial class OrderFollowUpEvaluateLog
+    {
+        public int ID { get; set; }
+        public long SoufunID { get; set; }
+        public int FollowUpID { get; set; }
+        public int IsFlower { get; set; }
+        public System.DateTime CreateTime { get; set; }
+        public int IsDel { get; set; }
+        public long OperSoufunID { get; set; }
+    }
+}
