738695dfd8e8db007a6daee63b7cc47e7f57d72a
AllenSteve <lacrima2012@qq.com>
2016-01-19 18:29:22 +0800

    01.新增DapperRow扩展方法单元测试类；

---
+++ StandardUnitTest/ExtensionTest/DapperRowExtensionTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExtensionComponent;
+using EOPComponent.Model;
+using EOPComponent.Model.ScoreAccumulation;
+using EOPComponent.Enums.Method;
+using System.Collections.Generic;
+
+namespace StandardUnitTest.ExtensionTest
+{
+    [TestClass]
+    public class DapperRowExtensionTest
+    {
+
+        private CompanyScoreOperationModel model { get; set; }
+
+        public DapperRowExtensionTest()
+        {
+
+        } 
+
+
+        [TestMethod]
+        public void QueryTest()
+        {
+            ScoreUpdateMethod method = new ScoreUpdateMethod();
+            object param = method.CreateQueryParam();
+
+            IEnumerable<object> list = method.Query(ScoreUpdateMethod.QueryFollowUpLogCompanyList(), param);
+            foreach(var item in list)
+            {
+                CompanyFollowUpModel result = item.ConvertDapperRowTo<CompanyFollowUpModel>();
+                
+            }
+
+            //// 查询业主日记反馈
+            //IEnumerable<CompanyScoreOperationModel> followUpResult = method.Query(ScoreUpdateMethod.QueryFollowUp(), method.remarkScore, param);
+            //// 查询工程质量验收合格记录
+            //IEnumerable<CompanyScoreOperationModel> constructResult = method.Query(ScoreUpdateMethod.QueryAcceptanceRecord(), method.qualifiedScore, param);
+        }
+    }
+}
