921e7da5033965124121a9cb9f317f1f27d8746c
AllenSteve <lacrima2012@qq.com>
2016-01-25 11:01:34 +0800

    01.同步EOP积分与公司积分字段；

---
+++ BaseComponent/CommonMethod/SyncEopCompanyScore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseComponent.CommonMethod
+{
+    public class SyncEopCompanyScore
+    {
+        public SyncEopCompanyScore()
+        {
+        }
+
+        /// <summary>
+        /// 执行公司积分同步
+        /// </summary>
+        public void UpdateCompanyScore()
+        {
+            string DB_CONNECTION = ConfigurationManager.ConnectionStrings["TestServerDB"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(DB_CONNECTION))
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(SyncEopCompanyScore.CreateUpdateSQL(), conn);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// 生成积分同步SQL
+        /// </summary>
+        /// <returns>SQL语句</returns>
+        private static string CreateUpdateSQL()
+        {
+            StringBuilder sql = new StringBuilder();
+            sql.Append(" UPDATE Partner_Company");
+            sql.Append(" SET Score = E.Score");
+            sql.Append(" FROM Partner_Company AS P");
+            sql.Append(" INNER JOIN dbo.Partner_CompanyExtent AS E");
+            sql.Append(" ON E.DealerID = P.ID");
+            return sql.ToString();
+        }
+    }
+}
