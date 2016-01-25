b19d1cbfd37f725bea75a8a6081d7d1c86afec86
AllenSteve <lacrima2012@qq.com>
2016-01-25 11:05:37 +0800

    01.新增控制台应用测试；

---
+++ StandardUnitTest/ComponentTest/Base.Component/SyncEopCompanyScoreTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BaseComponent.CommonMethod;
+
+namespace StandardUnitTest.ComponentTest.Base.Component
+{
+    [TestClass]
+    public class SyncEopCompanyScoreTest
+    {
+
+        SyncEopCompanyScore sync { get; set; }
+
+        public SyncEopCompanyScoreTest()
+        {
+            this.sync = new SyncEopCompanyScore();
+        }
+
+        [TestMethod]
+        public void SyncTest()
+        {
+            sync.UpdateCompanyScore();
+        }
+    }
+}
