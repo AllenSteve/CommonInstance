08e5828865c097f5355b01b6ec6c842760ab67b7
AllenSteve <lacrima2012@qq.com>
2016-01-26 09:44:52 +0800

    1.[装饰公司]带“保”字公司的口碑值不正确， 2.需求是在2000~3000之间随机赋值，现在超出了3000

--- BaseComponent/CommonMethod/SyncEopCompanyScore.cs
+++ BaseComponent/CommonMethod/SyncEopCompanyScore.cs
@@ -23,7 +23,7 @@ namespace BaseComponent.CommonMethod
             using (SqlConnection conn = new SqlConnection(DB_CONNECTION))
             {
                 conn.Open();
-                SqlCommand command = new SqlCommand(SyncEopCompanyScore.CreateUpdateSQL(), conn);
+                SqlCommand command = new SqlCommand(SyncEopCompanyScore.CreateRandomUpdateSQL(), conn);
                 command.ExecuteNonQuery();
             }
         }
@@ -37,6 +37,27 @@ namespace BaseComponent.CommonMethod
             StringBuilder sql = new StringBuilder();
             sql.Append(" UPDATE Partner_Company");
             sql.Append(" SET Score = E.Score");
+            sql.Append(" FROM Partner_Company AS P");
+            sql.Append(" INNER JOIN dbo.Partner_CompanyExtent AS E");
+            sql.Append(" ON E.DealerID = P.ID");
+            return sql.ToString();
+        }
+
+        /// <summary>
+        /// 生成积分同步SQL
+        /// </summary>
+        /// <returns>SQL语句</returns>
+        private static string CreateRandomUpdateSQL()
+        {
+            StringBuilder sql = new StringBuilder();
+            sql.Append(" UPDATE Partner_Company");
+            sql.Append(" SET Score =(");
+            sql.Append(" SET Score = E.Score");
+            sql.Append(" CASE CHARINDEX('保',P.CompanyName)");
+            sql.Append(" WHEN 0");
+            sql.Append(" THEN E.Score ");
+            sql.Append(" ELSE CAST(1000*(RAND()+2)  AS DECIMAL(38,0))");
+            sql.Append(" END)");
             sql.Append(" FROM Partner_Company AS P");
             sql.Append(" INNER JOIN dbo.Partner_CompanyExtent AS E");
             sql.Append(" ON E.DealerID = P.ID");
