fadf5236a01c16d3e18ce76b5e50c39a89d7dde2
AllenSteve <lacrima2012@qq.com>
2016-01-28 10:58:03 +0800

    新增函数重写单元测试方法；

---
+++ StandardUnitTest/ComponentTest/Base.Component/DoubleDispatchTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BaseComponent.DesignPatterns.DoubleDispatch;
+
+namespace StandardUnitTest.ComponentTest.Base.Component
+{
+    [TestClass]
+    public class DoubleDispatchTest
+    {
+        public DoubleDispatchTest()
+        { 
+        }
+
+        [TestMethod]
+        public void SurveyBaseTest()
+        {
+            SurveyBase instance = new Survey();
+            instance.DoSurvey();
+            Assert.AreEqual("Survey", instance.Target);
+        }
+
+        [TestMethod]
+        public void SurveyOverloadTest()
+        {
+
+        }
+    }
+}
