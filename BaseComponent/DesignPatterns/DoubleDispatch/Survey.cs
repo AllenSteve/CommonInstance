5b9e28411c23e78536216053300846bd9c4e5460
AllenSteve <lacrima2012@qq.com>
2016-01-28 10:40:10 +0800

    01.DoubleDispatch设计模式示例代码提交； 02.参照页面：http://www.cnblogs.com/HQFZ/p/4942561.html 03.简介： 说实话，我看过GoF《Design Patterns》，也曾深深的被李建忠《设计模式》系列Webcast吸引。但是还没有见过“Double Dispatch模式”。的确GoF提及的设计模式只是最初对设计模式的系统介绍，它不可能涵盖所有的模式。另外随着时间的流逝，技术日新月异的变化，技术大牛们又总结出了许多新的模式。
    今天所介绍的Double Dispatch模式，从时间上来看，已不是新的设计模式；但对于只看过GoF设计模式的技术同仁来说，也算是一个新的设计模式。

---
+++ BaseComponent/DesignPatterns/DoubleDispatch/Survey.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseComponent.DesignPatterns.DoubleDispatch
+{
+    /// <summary>
+    /// 来源：http://www.cnblogs.com/HQFZ/p/4942561.html
+    /// </summary>
+    public class Survey : SurveyBase
+    {
+        public override void DoSurvey()
+        {
+            Console.WriteLine("Derived Class");
+        }
+
+        public string DoSurvey(IAnimal animal)
+        {
+            return "IAnimal Type";
+        }
+
+        public string DoSurvey(Mammals animal)
+        {
+            return "Mammals Type";
+        }
+
+        public string DoSurvey(Dog animal)
+        {
+            return "Dog Type";
+        }
+    }
+}
---
+++ BaseComponent/DesignPatterns/DoubleDispatch/SurveyBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseComponent.DesignPatterns.DoubleDispatch
+{
+    public class SurveyBase
+    {
+        public virtual void DoSurvey()
+        {
+            Console.WriteLine("Base Class");
+        }
+    }
+}
