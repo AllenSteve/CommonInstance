28482fa48da678556e1fa5db3f7242009de8cd5c
AllenSteve <lacrima2012@qq.com>
2016-02-26 21:31:07 +0800

    更新测试方法；

--- StandardUnitTest/ExtensionTest/StringExtensionTest.cs
+++ StandardUnitTest/ExtensionTest/StringExtensionTest.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ExtensionComponent;
+using ExtensionComponent.String;
 using System.Diagnostics;
 using EOPComponent.Model.Entity;
 
