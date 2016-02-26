36590157bc90ff816e60d653359495cf2f6c0968
AllenSteve <lacrima2012@qq.com>
2016-02-26 21:30:36 +0800

    更新测试方法；

--- ComponentTest/ConsoleApp/StringTest.cs
+++ ComponentTest/ConsoleApp/StringTest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ExtensionComponent;
+using ExtensionComponent.String;
 using ComponentModels;
 using System.Web;
 
