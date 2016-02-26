aaf46c3050131a0bf96e90203463fdb140de167b
AllenSteve <lacrima2012@qq.com>
2016-02-26 21:13:03 +0800

    新增将类解析为string类型的方法；

---
+++ ExtensionComponent/StringExtension/StringParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionComponent.String
+{
+    public static partial class StringExtension
+    {
+        /// <summary>
+        /// 将对象类解析成string类型
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="str">The string.</param>
+        /// <param name="model">The model.</param>
+        /// <returns>System.String.</returns>
+        public static string ParseString<T>(this string str, T model)
+        {
+            Type type = typeof(T);
+            if (type.Name.Equals("String"))
+            {
+                return model.ToString();
+            }
+            else
+            {
+                PropertyInfo[] properties = type.GetProperties();
+                StringBuilder retStr = new StringBuilder();
+                foreach (var item in properties)
+                {
+                    retStr.Append(item.Name);
+                    retStr.Append("\t=\t");
+                    retStr.Append(item.GetValue(model, null));
+                    retStr.Append("\r\n");
+                }
+                return retStr.ToString();
+            }
+        }
+    }
+}
