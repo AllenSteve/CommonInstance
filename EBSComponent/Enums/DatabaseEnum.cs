9dbd501c3bdaff3bb675f6a61c8a8187a0470d5c
AllenSteve <lacrima2012@qq.com>
2016-03-01 16:00:06 +0800

    代码迁移，新增数据库枚举；

---
+++ EBSComponent/Enums/DatabaseEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbsComponent.Enums
+{
+    public enum DatabaseEnum
+    {
+        测试库只读 = 1,
+        测试库读写 = 2,
+        沙箱库只读 = 3,
+        沙箱库读写 = 4,
+        正式库只读 = 5,
+        正式库读写 = 6
+    }
+}
