059f28e19ad54dd824d958cee9565f3b9054f482
AllenSteve <lacrima2012@qq.com>
2016-03-01 16:10:50 +0800

    更新枚举类名称；

--- EBSComponent/Persistence/PersistenceEBS.cs
+++ EBSComponent/Persistence/PersistenceEBS.cs
@@ -8,6 +8,9 @@ namespace EBSComponent.Persistence
 {
     public class PersistenceEBS
     {
-
+        /// <summary>
+        /// 数据库连接字符串
+        /// </summary>
+        private string connectionStr { get; set; }
     }
 }
