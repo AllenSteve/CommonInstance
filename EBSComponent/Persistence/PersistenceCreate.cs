04c474c2a74cadf7ed562307e75ed32c0cc16b1c
AllenSteve <lacrima2012@qq.com>
2016-03-02 10:02:27 +0800

    添加批量新增记录的方法；

--- EBSComponent/Persistence/PersistenceCreate.cs
+++ EBSComponent/Persistence/PersistenceCreate.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using Dapper;
 using EBS.Interface.EContract.Method.EBSExtension;
 using System.Data;
+using System.Collections;
 
 namespace EBSComponent.Persistence
 {
@@ -50,6 +51,52 @@ namespace EBSComponent.Persistence
             else
             {
                 ret = this.writeConnection.Execute(sql.Insert<T>(), entity);
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// 新增实体对象集合到数据库
+        /// </summary>
+        /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="entityCollection">实体对象</param>
+        /// <param name="isTransaction">是否为事务操作</param>
+        /// <returns>返回结果</returns>
+        public int Add<T>(IEnumerable<T> entityCollection, bool isTransaction = false)
+        {
+            int ret = 0;
+            if (isTransaction)
+            {
+                if (this.writeConnection.State == ConnectionState.Closed)
+                {
+                    this.writeConnection.Open();
+                }
+                else
+                {
+                    using (IDbTransaction transaction = this.writeConnection.BeginTransaction())
+                    {
+                        try
+                        {
+                            foreach(var entity in entityCollection)
+                            {
+                                ret += this.writeConnection.Execute(sql.Insert<T>(), entity, transaction);
+                            }
+                            transaction.Commit();
+                        }
+                        catch (Exception ex)
+                        {
+                            ret = -1;
+                            transaction.Rollback();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (var entity in entityCollection)
+                {
+                    ret += this.writeConnection.Execute(sql.Insert<T>(), entity);
+                }
             }
             return ret;
         }
