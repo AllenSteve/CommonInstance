d625672438e537d71023640332537b806baf82a9
AllenSteve <lacrima2012@qq.com>
2016-03-07 00:07:53 +0800

    完成删除方法单元测试；

--- StandardUnitTest/ComponentTest/EBS.Component/PersistenceTest/DeleteTest.cs
+++ StandardUnitTest/ComponentTest/EBS.Component/PersistenceTest/DeleteTest.cs
@@ -1,15 +1,52 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EBSComponent.Persistence;
+using BaseComponent.Debugger;
+using EBSComponent.Enums;
+using ComponentModels.EbsDBModel;
 
 namespace StandardUnitTest.ComponentTest.EBS.Component.PersistenceTest
 {
     [TestClass]
     public class PersistenceDeleteTest
     {
+        private PersistenceEBS persistence { get; set; }
+        private DebugHelper debug { get; set; }
+
+        public PersistenceDeleteTest()
+        {
+            int databaseRead = (int)EntityTypeEnum.LOCAL_DATABASE;
+            int databaseWrite = (int)EntityTypeEnum.LOCAL_DATABASE;
+            this.persistence = new PersistenceEBS(databaseRead, databaseWrite);
+            this.debug = new DebugHelper();
+        }
+
         [TestMethod]
         public void DeleteTest()
         {
+            N_Order_Base order = new N_Order_Base();
+            int ret = 0;
+            //order = null;
+            //ret = persistence.Delete<N_Order_Base>(order);
+            //Assert.AreEqual(1,ret);
 
+            object param = new { ID = 1, Name = "test" };
+            ret = persistence.Delete<N_Order_Base>(null, param);
+            Assert.AreEqual(1, ret);
+        }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            N_Order_Base order = new N_Order_Base();
+            int ret = 0;
+            //order = null;
+            //ret = persistence.Remove<N_Order_Base>(order);
+            //Assert.AreEqual(1, ret);
+
+            object param = new { ID = 1, Name = "test" };
+            ret = persistence.Remove<N_Order_Base>(null, param);
+            Assert.AreEqual(1, ret);
         }
     }
 }
